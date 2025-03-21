4b9433f26406ed4c6c71e99f1e9b9ee1f706a3c4
Martha-KW <spielsachentests@gmail.com>
2025-03-21 19:58:56 +0100

    Unfinished main.py saved

---
+++ main.py
@@ -0,0 +1,12 @@
+import store
+import products
+
+# setup initial stock of inventory
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250)
+               ]
+best_buy = store.Store(product_list)
+
+def start(store):
+    pass
